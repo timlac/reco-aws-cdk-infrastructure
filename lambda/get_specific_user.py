d42fb32a9de0f7e67615a8a101cc238964d09b99
timlac <tim@lachmann.nu>
2023-12-04 11:45:53 +0100

    creating completely independent stacks

---
+++ lambda/get_specific_user.py
@@ -0,0 +1,47 @@
+import boto3
+import json
+import os
+from aws_lambda_powertools import Logger
+
+from boto3.dynamodb.conditions import Attr
+
+from serializer import to_serializable
+
+
+logger = Logger()
+
+
+def handler(event, context):
+    # Initialize DynamoDB client
+    dynamodb = boto3.resource('dynamodb')
+
+    user_id = event['pathParameters']['userId']
+
+    logger.info("user id:")
+    logger.info(user_id)
+
+    # Retrieve the DynamoDB table name from the environment variables
+    table_name = os.environ['DYNAMODB_TABLE_NAME']
+    table = dynamodb.Table(table_name)
+
+    try:
+        response = table.scan(
+            FilterExpression=Attr("id").eq(user_id)
+        )
+        items = response.get('Items', [])
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': True
+            },
+            'body': json.dumps(items, default=to_serializable)
+        }
+
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps(str(e))
+        }
--- stacks/emotion_categories_stack.py
+++ stacks/emotion_categories_stack.py
@@ -23,13 +23,13 @@ class EmotionCategoriesStack(Stack):
         api = api_stack.api
         authorizer = api_stack.authorizer
 
-        response_table = dynamodb.Table(self, "EmotionCategoriesResponseTable",
-                                        partition_key=dynamodb.Attribute(
-                                            name="id",
-                                            type=dynamodb.AttributeType.STRING
-                                        ),
-                                        billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
-                                        )
+        table = dynamodb.Table(self, "EmotionCategoriesResponseTable",
+                               partition_key=dynamodb.Attribute(
+                                   name="id",
+                                   type=dynamodb.AttributeType.STRING
+                               ),
+                               billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+                               )
 
         # This function needs to be adapted for each api
         create_user_lambda = lambda_.Function(
@@ -38,7 +38,7 @@ class EmotionCategoriesStack(Stack):
             handler="create_emotion_categories_user.handler",
             code=lambda_.Code.from_asset("lambda"),
             environment={
-                "DYNAMODB_TABLE_NAME": response_table.table_name
+                "DYNAMODB_TABLE_NAME": table.table_name
             },
             layers=[layer]
         )
@@ -47,25 +47,45 @@ class EmotionCategoriesStack(Stack):
         get_users_lambda = lambda_.Function(
             self, "GetUsersEmotionCategories",
             runtime=lambda_.Runtime.PYTHON_3_10,
-            handler="get_users.handler",
+            handler="get_user.handler",
             code=lambda_.Code.from_asset("lambda"),
             environment={
-                "DYNAMODB_TABLE_NAME": response_table.table_name
+                "DYNAMODB_TABLE_NAME": table.table_name
             },
             memory_size=512,
             layers=[layer]
         )
 
-        response_table.grant_read_write_data(create_user_lambda)
-        response_table.grant_read_data(get_users_lambda)
+        # This function is the same for both dbs
+        get_specific_user_lambda = lambda_.Function(
+            self, "GetSpecificUserEmotionCategories",
+            runtime=lambda_.Runtime.PYTHON_3_10,
+            handler="get_specific_user.handler",
+            code=lambda_.Code.from_asset("lambda"),
+            environment={
+                "DYNAMODB_TABLE_NAME": table.table_name
+            },
+            memory_size=512,
+            layers=[layer]
+        )
+
+        table.grant_read_write_data(create_user_lambda)
+        table.grant_read_data(get_users_lambda)
+        table.grant_read_data(get_specific_user_lambda)
 
         emotion_scales_users = api.root.add_resource("emotion_categories_users")
+
+        # backoffice endpoints
         emotion_scales_users.add_method("POST", apigateway.LambdaIntegration(create_user_lambda),
                                         authorizer=authorizer,
                                         authorization_type=apigateway.AuthorizationType.COGNITO
                                         )
-
         emotion_scales_users.add_method("GET", apigateway.LambdaIntegration(get_users_lambda),
                                         authorizer=authorizer,
                                         authorization_type=apigateway.AuthorizationType.COGNITO
                                         )
+
+        specific_user = emotion_scales_users.add_resource("{userId}")
+
+        # front-end endpoints
+        specific_user.add_method("GET", apigateway.LambdaIntegration(get_specific_user_lambda))
