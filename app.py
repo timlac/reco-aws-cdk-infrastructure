557fdacd5b6c6e6b4c789975b48d65cebd57a88d
timlac <tim@lachmann.nu>
2024-01-10 09:15:02 +0100

    removed videometadatastack

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import aws_cdk as cdk
 
 from stacks.cognito_stack import CognitoStack
 
-from stacks.video_metadata_stack import VideoMetadataStack
-
 from stacks.emotion_data_stack import EmotionDataStack
 
 app = cdk.App()
@@ -14,7 +12,5 @@ app = cdk.App()
 CognitoStack(app, "CognitoStack")
 
 EmotionDataStack(app, "EmotionDataStack")
-
-VideoMetadataStack(app, "VideoMetadataStack")
 
 app.synth()
